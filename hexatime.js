d479705729100531c57ca3bf1c73ffd4ea6c6f18
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-06-30 10:48:26 +0200

    Create hexatime.js

---
+++ hexatime.js
@@ -0,0 +1,28 @@
+/*
+
+  HexaTime.js 
+  ===========
+  2014 @LukyVj 
+  http://lucasbonomi.com
+  
+*/
+
+
+$(document).ready(function(){
+  // Start the function 
+  function hexatime_colorer(){
+    // Set an interval ( 1sec )
+    setInterval(function(){
+      var d = new Date(); // Get the date
+      var t = d.getHours() + d.getMinutes() + d.getSeconds(); // Format the date to get a plain string ( xxxxxx )
+      // Find and color the related classes
+      $('.hexatime_bg_color').css('background','#' + t) // Background
+      $('.hexatime_color').css('color','#' + t) // Color
+      $('.hexatime_br_color').css('color','#' + t) // Border Color
+    }, 1000);
+  }
+  function deploy(){
+    hexatime_colorer();
+  }
+ deploy();
+});
